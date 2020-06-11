72b43e59d8ea1290cd9be01fb86036aa76ee8ca3
Brennan Shacklett <bpshacklett@gmail.com>
2020-06-11 13:16:58 -0700

    no-as-needed to force the headless hack library to be linked

--- setup.py
+++ setup.py
@@ -9,5 +9,7 @@ setup(name='v4r_example',
       ext_modules=[CUDAExtension(name='v4r_example',
                                  sources=['v4r_example.cpp'],
                                  extra_compile_args=[f"-I{include_dir}"],
-                                 extra_link_args=[f"-L{lib_dir}", "-lv4r", "-lv4r_headless_hacks", f"-Wl,-rpath={lib_dir}"])],
+                                 extra_link_args=[f"-L{lib_dir}", "-lv4r",
+                                                  "-Wl,--no-as-needed", "-lv4r_headless_hacks",
+                                                  f"-Wl,-rpath={lib_dir}"])],
       cmdclass={'build_ext': BuildExtension})
