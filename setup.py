a4c8d03cd13ff46704349275c822a82edf0ff44e
erikwijmans <etw@gatech.edu>
2020-06-24 22:32:58 -0400

    My hacks

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 from setuptools import setup, Extension
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
-v4r_path = "/home/bps/rl/v4r"
+v4r_path = "/nethome/ewijmans3/projects/fast-rl-renderer/v4r"
 include_dir = v4r_path + "/include"
 lib_dir = v4r_path + "/build/src"
 
-setup(name='v4r_example',
-      ext_modules=[CUDAExtension(name='v4r_example',
-                                 sources=['v4r_example.cpp'],
-                                 extra_compile_args=[f"-I{include_dir}"],
-                                 extra_link_args=[f"-L{lib_dir}", "-lv4r",
-                                                  "-Wl,--no-as-needed", "-lv4r_headless_hacks",
-                                                  f"-Wl,-rpath={lib_dir}"])],
-      cmdclass={'build_ext': BuildExtension})
+setup(
+    name="v4r_example",
+    ext_modules=[
+        CUDAExtension(
+            name="v4r_example",
+            sources=["v4r_example.cpp"],
+            extra_compile_args=[f"-I{include_dir}"],
+            extra_link_args=[
+                f"-L{lib_dir}",
+                "-lv4r",
+                "-Wl,--no-as-needed",
+                "-lv4r_headless_hacks",
+                f"-Wl,-rpath={lib_dir}",
+            ],
+        )
+    ],
+    cmdclass={"build_ext": BuildExtension},
+)
--- v4r_example.cpp
+++ v4r_example.cpp
@@ -1,10 +1,11 @@
-#include <torch/extension.h>
 #include <ATen/ATen.h>
 #include <ATen/cuda/CUDAContext.h>
+#include <torch/extension.h>
 
 #include <cuda.h>
 #include <cuda_runtime.h>
 
+#include <glm/gtc/type_ptr.hpp>
 #include <v4r.hpp>
 
 #include <array>
@@ -16,85 +17,48 @@ using namespace v4r;
 
 namespace py = pybind11;
 
-static vector<glm::mat4> readViews(const string &p);
-
-constexpr uint32_t batch_size = 32;
-
 // Create a tensor that references this memory
-static at::Tensor convertToTensor(void *dev_ptr, int dev_id)
-{
-    array<int64_t, 4> sizes {{batch_size, 256, 256, 4}};
-
-    // This would need to be more precise for multi gpu machines
-    auto options = torch::TensorOptions().dtype(torch::kUInt8).
-        device(torch::kCUDA, (short)dev_id);
-
-    return torch::from_blob(dev_ptr, sizes, options);
-}
 
 class PyTorchSync {
-public:
-    PyTorchSync(RenderSync &&sync)
-        : sync_(move(sync))
-    {}
+  public:
+    PyTorchSync(RenderSync &&sync) : sync_(move(sync)) {}
 
-    void wait()
-    {
+    void wait() {
         // Get the current CUDA stream from pytorch and force it to wait
         // on the renderer to finish
         cudaStream_t cuda_strm = at::cuda::getCurrentCUDAStream().stream();
         sync_.gpuWait(cuda_strm);
     }
 
-private:
+  private:
     RenderSync sync_;
 };
 
-class V4RExample {
-public:
-    V4RExample(const string &scene_path, const string &views_path, int gpu_id)
-        : renderer_({
-              gpu_id,  // gpuID
-              1,  // numLoaders
-              1,  // numStreams
-              batch_size, // batchSize
-              256, // imgWidth,
-              256, // imgHeight
-              glm::mat4(
-                  1, 0, 0, 0,
-                  0, -1.19209e-07, -1, 0,
-                  0, 1, -1.19209e-07, 0,
-                  0, 0, 0, 1
-              ) // Habitat coordinate txfm matrix
-          }),
-          loader_(renderer_.makeLoader()),
-          cmd_strm_(renderer_.makeCommandStream()),
-          color_batch_(convertToTensor(cmd_strm_.getColorDevPtr(), gpu_id)),
-          views_(readViews(views_path)),
-          loaded_scenes_(),
-          view_cnt_(0)
-    {
-        loaded_scenes_.emplace_back(loader_.loadScene(scene_path));
-
-        envs.reserve(batch_size);
-
+struct V4RRenderGroup {
+    V4RRenderGroup(Unlit::CommandStream &&strm, std::shared_ptr<Scene> &scene,
+                   uint32_t batch_size, int gpu_id)
+        : cmd_strm_{std::move(strm)}, envs{}, color_batch_{convertToTensor(
+                                                  cmd_strm_.getColorDevPtr(),
+                                                  gpu_id, batch_size)} {
         for (uint32_t batch_idx = 0; batch_idx < batch_size; batch_idx++) {
-            envs.emplace_back(move(
-                cmd_strm_.makeEnvironment(loaded_scenes_.back(), 
-                                          90, 0.01, 1000)));
+            envs.emplace_back(
+                move(cmd_strm_.makeEnvironment(scene, 90, 0.01, 1000)));
         }
     }
 
-    ~V4RExample()
-    {
-    }
-
-    at::Tensor getColorTensor() const { return color_batch_; }
-
-    PyTorchSync render()
-    {
-        for (auto &env : envs) {
-            env.setCameraView(views_[view_cnt_++]);
+    PyTorchSync render(const std::vector<at::Tensor> &cameraPoses) {
+        AT_ASSERT(cameraPoses.size() == envs.size(),
+                  "Must have the same number of camera poses as batch_size");
+        {
+            int i = 0;
+            for (auto &env : envs) {
+                AT_ASSERT(!cameraPoses[i].is_cuda(), "Must be on CPU");
+                AT_ASSERT(cameraPoses[i].scalar_type() == at::ScalarType::Float,
+                          "Must be float32");
+                glm::mat4 pose = glm::transpose(
+                    (glm::make_mat4(cameraPoses[i++].data_ptr<float>())));
+                env.setCameraView(pose);
+            }
         }
 
         auto sync = cmd_strm_.render(envs);
@@ -102,42 +66,81 @@ public:
         return PyTorchSync(move(sync));
     }
 
-private:
-    Unlit::BatchRenderer renderer_;
-    Unlit::AssetLoader loader_;
+  private:
     Unlit::CommandStream cmd_strm_;
-    at::Tensor color_batch_;
-    vector<glm::mat4> views_;
-    vector<shared_ptr<Scene>> loaded_scenes_;
-    uint64_t view_cnt_;
     vector<Environment> envs;
-};
 
-vector<glm::mat4> readViews(const string &p)
-{
-    ifstream dump_file(p, ios::binary);
+    at::Tensor convertToTensor(void *dev_ptr, int dev_id, uint32_t batch_size) {
+        std::cout << batch_size << ", " << dev_id << std::endl;
+        std::cout << dev_ptr << std::endl;
+        array<int64_t, 4> sizes{{batch_size, 256, 256, 4}};
 
-    vector<glm::mat4> views;
+        // This would need to be more precise for multi gpu machines
+        auto options = torch::TensorOptions()
+                           .dtype(torch::kUInt8)
+                           .device(torch::kCUDA, (short)dev_id);
 
-    for (size_t i = 0; i < 30000; i++) {
-        float raw[16];
-        dump_file.read((char *)raw, sizeof(float)*16);
-        views.emplace_back(glm::inverse(
-                glm::mat4(raw[0], raw[1], raw[2], raw[3],
-                          raw[4], raw[5], raw[6], raw[7],
-                          raw[8], raw[9], raw[10], raw[11],
-                          raw[12], raw[13], raw[14], raw[15])));
+        std::cout << "Before from_blob" << std::endl;
+        auto t = torch::from_blob(dev_ptr, sizes, options);
+        std::cout << "End from_blob" << std::endl;
+
+        return t;
     }
 
-    return views;
-}
+  public:
+    at::Tensor color_batch_;
+};
+
+class V4RExample {
+  public:
+    V4RExample(const std::vector<std::string> &scene_paths, uint32_t batch_size,
+               int gpu_id)
+        : renderer_({
+              gpu_id, // gpuID
+              1,      // numLoaders
+              2,      // numStreams
+              batch_size,
+              256, // imgWidth,
+              256, // imgHeight
+              glm::mat4(1, 0, 0, 0, 0, -1.19209e-07, -1, 0, 0, 1, -1.19209e-07,
+                        0, 0, 0, 0, 1) // Habitat coordinate txfm matrix
+          }),
+          loader_(renderer_.makeLoader()), loaded_scenes_() {
+        for (auto &scene_path : scene_paths) {
+            loaded_scenes_.emplace_back(loader_.loadScene(scene_path));
+        }
+
+        for (int i = 0; i < 2; ++i) {
+            renderGroups_.emplace_back(
+                new V4RRenderGroup(std::move(renderer_.makeCommandStream()),
+                                   loaded_scenes_[i], batch_size, gpu_id));
+        }
+    }
+
+    ~V4RExample() = default;
+
+    at::Tensor getColorTensor(const uint32_t groupIdx) const {
+        return renderGroups_[groupIdx]->color_batch_;
+    }
+
+    PyTorchSync render(const std::vector<at::Tensor> &cameraPoses,
+                       const uint32_t groupIdx) {
+        return std::move(renderGroups_[groupIdx]->render(cameraPoses));
+    }
+
+  private:
+    Unlit::BatchRenderer renderer_;
+    Unlit::AssetLoader loader_;
+    vector<shared_ptr<Scene>> loaded_scenes_;
+
+    vector<std::unique_ptr<V4RRenderGroup>> renderGroups_;
+};
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     py::class_<V4RExample>(m, "V4RExample")
-        .def(py::init<const string &, const string &, int>())
+        .def(py::init<const std::vector<string> &, int, int>())
         .def("render", &V4RExample::render)
-        .def("getColorTensor", &V4RExample::getColorTensor);
+        .def("rgba", &V4RExample::getColorTensor);
 
-    py::class_<PyTorchSync>(m, "PyTorchSync")
-        .def("wait", &PyTorchSync::wait);
+    py::class_<PyTorchSync>(m, "PyTorchSync").def("wait", &PyTorchSync::wait);
 }
